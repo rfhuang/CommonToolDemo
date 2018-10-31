c3dd8da998a6fd6eb9da5d0bb5aa5c3ee439df34
rfhuang <yifeng544@163.com>
2018-10-31 14:31:59 +0800

    rfhuang
    提交工程代码

---
+++ src/main/java/com/lucky/CommonToolDemoApplication.java
@@ -0,0 +1,12 @@
+package com.lucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonToolDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonToolDemoApplication.class, args);
+	}
+}
